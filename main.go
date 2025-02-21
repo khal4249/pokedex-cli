2627e7ec67b49508979f0d56e213a25f6330d040
khal4249 <kandishahall@gmail.com>
2025-02-21 21:17:43 +0000

    added basic repl functionality

--- main.go
+++ main.go
@@ -1,9 +1,77 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("Pokedex > ")
+		scanner.Scan()
+		line := scanner.Text()
+		cleanLine := cleanInput(line)
+		//fmt.Printf("Your command was: %s\n", cleanLine[0])
+		command := cleanLine[0]
+		commands := returnCommands()
+		values, ok := commands[command]
+		if ok {
+			values.callback()
+			//err := values.callback
+			//fmt.Printf("%v\n", err)
+		} else {
+			fmt.Printf("Unknown command\n")
+		}
+	}
+	/*if err := scanner.Err(); err != nil {
+	    fmt.Fprintln(os.Stderr, "error:", err)
+	} */
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func cleanInput(text string) []string {
+	cleaned := strings.ToLower(text)
+	cleanedSlice := strings.Fields(cleaned)
+	return cleanedSlice
+}
+
+func commandExit() error {
+	fmt.Printf("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	commands := returnCommands()
+	for command, desc := range commands {
+		fmt.Printf("%s: %s\n", command, desc.description)
+	}
+	return nil
+}
+
+func returnCommands() map[string]cliCommand {
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+	return commands
 }
---
+++ repl_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	//"fmt"
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+
+		{
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+		{
+			input:    "word",
+			expected: []string{"word"},
+		},
+		// add more cases here
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+			if word != expectedWord {
+				t.Errorf("Got word %q, expected %q", word, expectedWord)
+			}
+		}
+	}
+}
